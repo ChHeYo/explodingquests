e23e43e9bdac581487269b6980b1384aa26dd03a
ChHeYo <cyoo3@u.rochester.edu>
2017-07-20 18:41:35 -0400

    added quest model

--- explodingquests/settings/base.py
+++ explodingquests/settings/base.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'explodingquests.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [TEMPLATES_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
--- quests/models.py
+++ quests/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Quest(models.Model):
+    DIFFICULTIES = (
+        ('E', 'EASY'),
+        ('M', 'MEDIUM'),
+        ('H', 'HARD'),
+    )
+
+    npc             = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title           = models.CharField(max_length=255)
+    description     = models.CharField(max_length=255)
+    objectives      = models.CharField(max_length=255)
+    difficulty      = models.CharField(max_length=1, choices=DIFFICULTIES)
+    rewards         = models.CharField(max_length=255)
+    date_created    = models.DateTimeField(auto_now_add=True)
+    date_modified   = models.DateTimeField(auto_now=True)
+    explosion_date  = models.DateTimeField()
+    slug            = models.SlugField()
+
+    class Meta:
+        ordering = ['-date_created']    
+
+    def __str__(self):
+        return self.title
